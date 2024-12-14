ac6bf089f1f02dad394808d0230e446b3b5b857a
Oliver Malele <malelemdz@gmail.com>
2024-12-14 02:05:47 -0400

    Update app-admin.js

--- app-admin.js
+++ app-admin.js
@@ -1,5 +1,5 @@
 // URL de la base de datos
-const baseUrl = 'https://letras.malelemdz.com/db.json';
+const baseUrl = 'http://letras.malelemdz.com/db.json';
 
 // Selección de elementos del DOM
 const form = document.getElementById('song-form');
