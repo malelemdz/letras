b3022dc3bc4883675b6566ce68c7a33608c8fbd0
Oliver Malele <malelemdz@gmail.com>
2024-12-15 00:44:40 -0400

    Delete auto-resize.js

--- auto-resize.js
+++
@@ -1,9 +0,0 @@
-// Ajustar textarea automáticamente
-document.querySelectorAll('textarea').forEach(textarea => {
-    textarea.style.overflow = 'hidden';  // Oculta el scroll
-    textarea.style.resize = 'none';  // Evita el redimensionamiento manual
-    textarea.addEventListener('input', function () {
-        this.style.height = 'auto';  // Resetea la altura para recalcular
-        this.style.height = this.scrollHeight + 'px';  // Ajusta la altura al contenido
-    });
-});
